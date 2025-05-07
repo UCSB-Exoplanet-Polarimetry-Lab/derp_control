18a50234100513fcc0471b0508921ea8df28ba8b
William Melby <wcmelby@ucsb.edu>
2025-05-07 14:19:02 -0700

    adding photodiode code

--- derpy/camera.py
+++ derpy/camera.py
@@ -1,4 +1,3 @@
-import FliSdk_V2 as sdk
 import time
 from astropy.io import fits
 from .derpy_conf import (
@@ -7,6 +6,16 @@ from .derpy_conf import (
     CAMERA_TEMP_READOUT_DELAY,
     VERBOSE
 )
+from warnings import warn
+
+from  .photodiode_class import OPM
+
+try:
+    import FliSdk_V2 as sdk
+except ImportError:
+    warn("FliSdk_V2 not found. Make sure the SDK is installed and in your PYTHONPATH.")
+
+
 
 def display_all_temps(context,verbose = True):
     res, mb, fe, pw, sensor, peltier, heatsink = sdk.FliCredTwo.GetAllTemp(context)
@@ -73,18 +82,17 @@ class CRED2:
 
         sensor_temp = display_all_temps(self.context)
         
-        # TODO: Include np.isclose() to check if the temperature is within tolerance
-        if self.set_temp < initial_temp:
-            while sensor_temp > self.set_temp:
-                sensor_temp = display_all_temps(self.context)
-                self.temperature_change.append(sensor_temp)
-                time.sleep(CAMERA_TEMP_READOUT_DELAY)
 
-        elif self.set_temp > initial_temp:
-            while sensor_temp < self.set_temp:
+        # Use np.isclose() to check if the temperature is within tolerance
+        tol = temp_tolerance # absolute tolerance
+        rtol = 0 # relative tolerance (optional)
+
+        if sensor_temp != self.set_temp:
+            while not np.isclose(self.set_temp, sensor_temp, rtol=rtol, atol=tol):
                 sensor_temp = display_all_temps(self.context)
                 self.temperature_change.append(sensor_temp)
-                time.sleep(CAMERA_TEMP_READOUT_DELAY)
+                time.sleep(CAMERA_TEMP_READOUT_DELAY)           
+
 
         self.sensor_temp = display_all_temps(self.context, verbose=False)
         print(f'Final Sensor Temperature {self.sensor_temp:.2f}C')
@@ -131,8 +139,8 @@ class CRED2:
 
             res, response = sdk.FliSerialCamera.SendCommand(self.context, "tint raw")
             tint = response * 1000
-            if VERBOSE:
-                print(f"Prior camera tint: {tint}ms")
+            #if VERBOSE:
+                #print(f"Prior camera tint: {tint}ms")
 
             mintint = self.min_tint
             maxtint = self.max_tint
@@ -163,7 +171,7 @@ class CRED2:
         self._conversion_gain = value
         res = sdk.FliCredTwo.SetConversionGain(self.context, value)
 
-    def take_many_images(self, num_images, save_path=None, verbose=False):
+    def take_many_images(self, num_images, save_path=None, verbose=False, OPM=None):
         """take many images and save them to a directory
 
         Parameters
@@ -180,9 +188,18 @@ class CRED2:
 
         frame_list = []
 
+        if OPM != None:
+            power_list = []
+        elif OPM == None:
+            power_list = None
+
         for i in range(num_images):
             frame = sdk.GetRawImageAsNumpyArray(self.context, 0).astype(np.float64)
             frame_list.append(frame)
+
+            if OPM != None:
+                power = OPM.get_reading()
+                power_list.append(power)
 
             if verbose:
                 print(f"Image {i} taken")
@@ -197,11 +214,44 @@ class CRED2:
             hdul = fits.HDUList([hdu])
             hdul.writeto(save_path, overwrite=True)
 
-        return frame_list
+        return frame_list, power_list
 
-    def take_image(self, save_path=None, verbose=False):
+    def take_median_image(self, n_frames, save_path=None, verbose=False, OPM = None):
+        frame_list, power_list = self.take_many_images(n_frames, save_path=save_path, verbose=verbose, OPM = OPM,) 
+        frame_list_median = np.median(frame_list, axis=0)
 
-        frame_list = self.take_many_images(1, save_path=save_path, verbose=verbose)
+        if save_path is not None:
+            hdu = fits.PrimaryHDU(frame_list_median)
+            hdul = fits.HDUList([hdu])
+            hdul.writeto(f'{save_path}_median', overwrite=True) # overwrites original, non-median-combined image
+
+        return frame_list_median 
+    
+    def take_mean_image(self, n_frames, save_path=None, verbose=False, OPM = None):
+        frame_list, power_list = self.take_many_images(n_frames, save_path=save_path, verbose=verbose, OPM = OPM) 
+        frame_list_mean = np.mean(frame_list, axis=0)
+
+        if save_path is not None:
+            hdu = fits.PrimaryHDU(frame_list_mean)
+            hdul = fits.HDUList([hdu])
+            hdul.writeto(f'{save_path}_mean', overwrite=True) # overwrites original, non-mean-combined image
+
+        return frame_list_mean 
+    
+    def take_std_image(self, n_frames, save_path=None, verbose=False, OPM = None):
+        frame_list, power_list = self.take_many_images(n_frames, save_path=save_path, verbose=verbose, OPM = OPM) 
+        frame_list_std = np.std(frame_list, axis=0)
+
+        if save_path is not None:
+            hdu = fits.PrimaryHDU(frame_list_std)
+            hdul = fits.HDUList([hdu])
+            hdul.writeto(f'{save_path}_std', overwrite=True) # overwrites original, non-std-combined image
+
+        return frame_list_std 
+
+    def take_image(self, save_path=None, verbose=False, OPM = None):
+
+        frame_list = self.take_many_images(1, save_path=save_path, verbose=verbose, OPM = OPM)
         return frame_list
 
     def close(self):
