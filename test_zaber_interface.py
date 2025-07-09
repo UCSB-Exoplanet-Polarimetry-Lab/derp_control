cb368f98a520c1c04b50edb164cffef1846a2a2d
Jashcraf <jarenashcraft@ucsb.edu>
2025-07-09 09:56:23 -0700

    another cleanup

--- test_zaber_interface.py
+++ test_zaber_interface.py
@@ -1,8 +1,13 @@
-from derpy.motion import BaseZaberStage
+from derpy.motion import BaseZaberStage, ZaberConnection
+
+zaber = ZaberConnection("COM4")
 
 # init stage
-zaber = BaseZaberStage("COM4", 0)
+zaber1 = BaseZaberStage(zaber, 0)
+zaber2 = BaseZaberStage(zaber, 1)
 
-zaber.step(25)
-zaber.close()
+zaber1.step(25)
+zaber2.step(25)
+zaber1.close()
+zaber2.close()
 
